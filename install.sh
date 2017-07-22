44e6ea21e905cff7c4695c265e9634daef97ebce
TarryPaloma <beyer.dynamics@gmail.com>
2017-07-22 17:50:45 +0100

    added sed for server monit script

--- install.sh
+++ install.sh
@@ -45,6 +45,8 @@ git clone https://gitlab.nanos.io/jc3mp-packages/chat
  
 mkdir /home/"$username"/jc3mp/"$svalias"/monit
 git clone https://github.com/TarryPaloma/jc3mp-linux-server-monit /home/"$username"/jc3mp/"$svalias"/monit
+sed -i "s/NAME='replaceme'/NAME='$alias'/" /home/"$username"/jc3mp/"$svalias"/monit/alias.sh
+sed -i "s/DIR=/home/'replaceme'/DIR=/home/'$username'/" /home/"$username"/jc3mp/"$svalias"/monit/alias.sh
 mv /home/"$username"/jc3mp/"$svalias"/monit/alias.sh /home/"$username"/jc3mp/"$svalias"/monit/"$svalias".sh
 touch /home/"$username"/jc3mp/"$svalias"/monit/"$svalias".pid /home/"$username"/jc3mp/"$svalias"/monit/"$svalias".log
 
