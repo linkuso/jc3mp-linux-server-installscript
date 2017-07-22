e4bc94da8471fd3f8a5feeaced20eaf88ec38a4d
TarryPaloma <beyer.dynamics@gmail.com>
2017-07-22 16:44:32 +0100

    woops, we need to make the /jc3mp/ directory first

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ else
         useradd -m -u 1337 -g users -d /home/"$username" -s /bin/bash -p $(echo "$password" | openssl passwd -1 -stdin) "$username"
 fi
 
+su "$username" -c "mkdir /home/'$username'/jc3mp/
 su "$username" -c "mkdir /home/'$username'/jc3mp/'$svalias'"
 su "$username" -c "steamcmd +login anonymous +exit"
 su "$username" -c "steamcmd +login anonymous +force_install_dir /home/'$username'/jc3mp/"$svalias" +app_update 619960 validate +exit"
