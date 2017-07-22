4402dc63441dd200f901111da60f5242e3e207e2
Linkus <linusolausson@gmail.com>
2017-07-22 17:43:42 +0200

    fixed monitrc edit

--- install.sh
+++ install.sh
@@ -66,9 +66,9 @@ sed -i "/admins: \[/a \\\t'$adminsteamid'," /home/"$username"/jc3mp/"$svalias"/p
 sed -i "/death_reasons: \[/a \\\t'tickled the belly of',\n \\t'popped a cherry in',\n \\t'fragged',\n \\t'mutilated',\n \\t'720 noscoped',\n \\t'gatted',\n \\t'bamboozled',\n \\t'mullered',\n \\t'inflicted mortal damage upon',\n \\t'erased',\n \\t'julienned',\n \\t'killded',\n \\t'punctured',\n \\t'perforated',\n \\t'deaded'," /home/"$username"/jc3mp/"$svalias"/packages/freeroam/gm/config.js
 
 cat >> /etc/monit/monitrc <<EOF
-check process jc3mpServer_$svalias with pidfile /home/"$username"/jc3mp/"$svalias"/monit/"$svalias".pid
-start program = "/bin/su '$username' -c '/home/'$username'/jc3mp/'$svalias'/monit/$svalias.sh start'"
-stop program = "/bin/su '$username' -c '/home/'$username'/jc3mp/'$svalias'/monit/$svalias.sh stop'"
+check process jc3mpServer_$svalias with pidfile /home/$username/jc3mp/$svalias/monit/$svalias.pid
+start program = "/bin/su $username -c '/home/$username/jc3mp/$svalias/monit/$svalias.sh start'"
+stop program = "/bin/su $username -c '/home/$username/jc3mp/$svalias/monit/$svalias.sh stop'"
 EOF
 
 chown -R "$username":users /home/"$username"/jc3mp
